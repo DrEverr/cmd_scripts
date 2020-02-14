62b42e3926a96038ddd6500a5fe2d6193f424ae3
Marcin "Staś" Sokołowski <ms1997sokol@gmail.com>
2020-02-14 16:40:29 +0100

    Added TO DO comment

--- return.cmd
+++ return.cmd
@@ -1,4 +1,7 @@
 @ECHO OFF
+:: Use this script after gained errors to know more about particular error
+:: TO DO
+  :: Add more error codes
 ECHO return = %ERRORLEVEL%
 IF %ERRORLEVEL% EQU 9009 (
   ECHO error - SomeFile.exe not found in your PATH
